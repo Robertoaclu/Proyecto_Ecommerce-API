e96078155bfa2a91280df4b84fb3a7a4c27d33e2
Robertoaclu <robertoaclu@gmail.com>
2023-11-23 17:50:00 +0100

    Establecer conexion con la base de datos

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require(`express`);
 const app = express();
+const mysql = require("mysql2");
 
 app.use(express.static('public'));
+app.use(express.json());
+
+// -----------------------------------------------------------------------------------
+// MySQL connection
+
+const connection = mysql.createConnection({
+    host: "localHost",
+    user: "root",
+    password: "Elitista1990",
+    database: "sneaker_store",
+});
+
+connection.connect(function (error) {
+    if (error) {
+        return console.error(`error: ${error.message}`);
+    }
+    console.log("Conectando a MySQL!!!");
+});
+
+// -----------------------------------------------------------------------------------
+
+
+
+
 
 app.get("/hello", (request, response) => {
     response.send({message:"hello world!"});
