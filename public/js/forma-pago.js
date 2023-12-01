a914d8246e84c6eedcecacdf5b51524c680beff5
Robertoaclu <robertoaclu@gmail.com>
2023-12-01 14:10:05 +0100

    upgrade files

--- public/js/forma-pago.js
+++ public/js/forma-pago.js
@@ -1,27 +1,31 @@
 const host = "http://localhost:8000";
 
-window.addEventListener("load", formasPago());
+window.addEventListener("load", ()=> {formasPago(1)});
 
 // ----------------------------------------------------------------------------
 // Script para mostrar la lista de tarjetas en la forma de pago (GET)
 
+let totalTarjetas=0;
 function formasPago(usuarioId){
-    fetch(`${host}/forma-pago/${usuarioId}`)
+    fetch(`${host}/formasPago/${usuarioId}`)
     .then(function(response){
         return response.json();
     })
     .then(function(json){
-        let formasPagoDiv= document.getElementById("formasPago")
-
-        let innerHTML="<div>";
+        let formasPagoDiv= document.getElementById("listaTarjetas")
+        
+        let innerHTML="";
         for(i=0; i<json.length; i++){
-            innerHTML +=
-            `<div id="">
-                <p>${json[i].id}</p>
+          innerHTML +=
+            `<div class="cajaTarjeta">
+            <label for="tarjeta_${i}" >${json[i].numero_tarjeta}</label>
+            <input type="radio" id="tarjeta_${i}" name="tarjeta" value="${json[i].id}" />
             </div>`;
         }
-        <button class="boton" onClick="location.href='html/finalizar-pedido.html'">Pagar con esta tarjeta</button>
-        innerHTML += "</div>";
+        innerHTML+=`<button onclick="seleccionarTarjeta()">Pagar con esta tarjeta</button>`;
+        
+        totalTarjetas=json.length;
+        
 
         formasPagoDiv.innerHTML=innerHTML;
     }).catch(function(error){
@@ -29,6 +33,11 @@ function formasPago(usuarioId){
     });
 }
 
-function tarjetaPago(){
-    localStorage.setItem("tarjeta", )
+function seleccionarTarjeta(){
+    for (i=0; i<totalTarjetas; i++){
+        let seleccionada=document.getElementById(`tarjeta_${i}`)
+        if(seleccionada.checked){
+            localStorage.setItem("tarjeta", json[i].id);
+        }
+    }
 }
